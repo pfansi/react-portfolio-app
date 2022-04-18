3b1b3426cb59d70309bc114eba7302c95726ca20
pfansiW <fancyfreelance76@gmail.com>
2022-04-19 00:38:58 +0100

    Navbar component created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Work from "./components/Work";
+
 function App() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
